97a9f6f1b657d97a4c3be262fd9f8ea387f1ee0f
Nafisah Nubah <Nafisah.Nubah@dal.ca>
2024-12-09 00:30:35 -0400

    made deployment related changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
+  tls: true,
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -20,63 +21,82 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
+function run() {
+//   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const movieCollection = client.db('movieDB').collection('movie');
     const favMovieCollection = client.db('movieDB').collection('fav-movie');
     const userCollection = client.db('movieDB').collection('user');
 
-    app.get('/addmovie', async(req, res) => {
-      const cursor = movieCollection.find().sort( { "rating": -1 } );
-      const result = await cursor.toArray();
-      res.send(result);
+    app.get('/addmovie', (req, res) => {
+      const cursor = movieCollection.find().sort( { "rating": -1 } ).toArray()
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error retrieving movies.' });
+        });
     });
 
-    app.get('/movie/:id', async(req, res) => {
+    app.get('/movie/:id', (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
-      const result = await movieCollection.findOne(query);
-      res.send(result);
+      movieCollection.findOne(query)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error retrieving the movie.' });
+        });
     });
 
-    app.post('/addmovie', async(req, res) => {
-        const newMovie = req.body;
-        console.log(newMovie)
-        const result = await movieCollection.insertOne(newMovie);
-        res.send(result);
+    app.post('/addmovie', (req, res) => {
+      const newMovie = req.body;
+      movieCollection.insertOne(newMovie)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error adding the movie.' });
+        });
     });
 
-    app.get('/favourites', async(req, res) => {
-      const cursor = favMovieCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
+    app.get('/favourites', (req, res) => {
+      favMovieCollection.find().toArray()
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error retrieving favorites.' });
+        });
     });
 
-    app.get('/favourites/:id', async(req, res) => {
+    app.get('/favourites/:id', (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
-      const result = await favMovieCollection.findOne(query);
-      res.send(result);
+      favMovieCollection.findOne(query)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error retrieving the favorite movie.' });
+        });
     });
 
-    app.post('/favourites/:id', async(req, res) => {
+    app.post('/favourites/:id', (req, res) => {
       const favMovie = req.body;
-      console.log(favMovie)
-      const result = await favMovieCollection.insertOne(favMovie);
-      res.send(result);
+      favMovieCollection.insertOne(favMovie)
+        .then(result => {
+          res.send(result);
+        })
+        .catch(error => {
+          res.status(500).send({ error: 'Error inserting the favorite movie.' });
+        });
     });
 
-    app.delete('/favourites/:id', async(req, res) => {
+    app.delete('/favourites/:id', (req, res) => {
       const id = req.params.id;
       const query = {_id: id}
-      const result = await favMovieCollection.deleteOne(query);
-      res.send(result);
+      favMovieCollection.deleteOne(query)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error deleting the favorite movie.' });
+        });
     });
 
-    app.put('/movie/:id', async(req, res) => {
+    app.put('/movie/:id', (req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const options = {upsert : true}
@@ -94,32 +114,43 @@ async function run() {
         }
       }
 
-      const result = await movieCollection.updateOne(filter, movie, options);
-      res.send(result);
+      movieCollection.updateOne(filter, movie, options)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error updating the movie.' });
+        });
     })
 
-    app.delete('/movie/:id', async(req, res) => {
+    app.delete('/movie/:id', (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
-      const result = await movieCollection.deleteOne(query);
-      res.send(result);
+      movieCollection.deleteOne(query)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error deleting the movie.' });
+        });
     });
 
-    app.post('/users', async(req, res) => {
+    app.post('/users', (req, res) => {
       const newUser = req.body;
-      const result = await userCollection.insertOne(newUser);
-      res.send(result);
+      userCollection.insertOne(newUser)
+        .then(result => res.send(result))
+        .catch(error => {
+          res.status(500).send({ error: 'Error adding the user.' });
+        });
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
+
+run();
 
 
 
